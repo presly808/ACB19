c11b1665d769e7c214b50dd99daf633640178a0c
presly808 <presly808@gmail.com>
2016-10-10 09:16:58 +0300

    week1 home

---
+++ src/ua/artcode/Main.java
@@ -0,0 +1,24 @@
+package ua.artcode;
+
+/**
+ * Created by serhii on 10/9/16.
+ */
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Input age");
+        int age = sc.nextInt();
+
+        System.out.println("Input name");
+        String name = sc.next();
+
+        System.out.println("Hello");
+    }
+
+
+}
